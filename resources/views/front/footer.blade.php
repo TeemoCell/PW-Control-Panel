6e16bf8d26195e0c9d94b9ded203bfa30b5bf116
TeemoCell <11705513+TeemoCell@users.noreply.github.com>
2021-01-09 18:54:43 +0100

    #

--- resources/views/front/footer.blade.php
+++ resources/views/front/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- BEGIN INNER FOOTER -->
 <div class="page-footer">
     <div class="container"> Support the developers of the panel.
-        <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=R9FB8E4X5EYVC" target="_blank">Donate!</a>
+        <a href="https://paypal.me/UnturnedOne" target="_blank">Donate!</a>
     </div>
 </div>
 <div class="scroll-to-top">
